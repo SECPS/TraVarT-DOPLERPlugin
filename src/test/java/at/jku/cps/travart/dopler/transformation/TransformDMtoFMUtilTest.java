9498e67be14d3c703280645f1d80778cdba403e6
Dario Romano <d.salvo.romano@gmail.com>
2023-01-17 15:17:42 +0100

    initialize tests for DM to FM

--- src/test/java/at/jku/cps/travart/dopler/transformation/TransformDMtoFMUtilTest.java
+++ src/test/java/at/jku/cps/travart/dopler/transformation/TransformDMtoFMUtilTest.java
@@ -5,15 +5,82 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import at.jku.cps.travart.dopler.decision.IDecisionModel;
+import at.jku.cps.travart.dopler.decision.exc.CircleInConditionException;
+import at.jku.cps.travart.dopler.decision.factory.impl.DecisionModelFactory;
+import at.jku.cps.travart.dopler.decision.model.impl.BooleanDecision;
+import at.jku.cps.travart.dopler.decision.model.impl.Cardinality;
+import at.jku.cps.travart.dopler.decision.model.impl.DoubleValue;
+import at.jku.cps.travart.dopler.decision.model.impl.EnumDecision;
+import at.jku.cps.travart.dopler.decision.model.impl.NumberDecision;
+import at.jku.cps.travart.dopler.decision.model.impl.Range;
+import at.jku.cps.travart.dopler.decision.model.impl.StringDecision;
+import at.jku.cps.travart.dopler.decision.model.impl.StringValue;
+import at.jku.cps.travart.dopler.transformation.roundtrip.TransformDMtoFMUtil;
+import de.vill.model.FeatureModel;
+
 class TransformDMtoFMUtilTest {
+	DecisionModelFactory factory;
+	IDecisionModel dm;
+	BooleanDecision cream;
+	EnumDecision holder;
+	NumberDecision portions;
+	EnumDecision flavors;
+	StringDecision name;
+	EnumDecision toppings;
+	
+	FeatureModel fm;
 
 	@BeforeEach
 	void setUp() throws Exception {
+		dm=factory.getInstance().create();
+		cream = new BooleanDecision("whipped cream?");
+		holder= new EnumDecision("What kind of holder?");
+		Range<String> holderRange= new Range<>();
+		holderRange.add(new StringValue("Edible cup"));
+		holderRange.add(new StringValue("plastic cup"));
+		holderRange.add(new StringValue("paper cup"));
+		holder.setRange(holderRange);
+		holder.setCardinality(new Cardinality(1,1));
+		portions= new NumberDecision("how many portions?");
+		Range<Double> portionRange= new Range<>();
+		portionRange.add(new DoubleValue(1));
+		portionRange.add(new DoubleValue(2));
+		portionRange.add(new DoubleValue(3));
+		portionRange.add(new DoubleValue(4));
+		portions.setRange(portionRange);
+		flavors= new EnumDecision("Flavors?");
+		flavors.setCardinality(new Cardinality(1,4));
+		Range<String> flavorRange=new Range<>();
+		flavorRange.add(new StringValue("vanilla"));
+		flavorRange.add(new StringValue("strawberry"));
+		flavorRange.add(new StringValue("chocolate"));
+		flavorRange.add(new StringValue("hazelnut"));
+		flavorRange.add(new StringValue("banana"));
+		flavorRange.add(new StringValue("bubblegum"));
+		flavors.setRange(flavorRange);
+		toppings= new EnumDecision("Toppings?");
+		Range<String> topRange= new Range<>();
+		topRange.add(new StringValue("rainbow sprinkles"));
+		topRange.add(new StringValue("cocoa"));
+		topRange.add(new StringValue("Smarties"));
+		topRange.add(new StringValue("gold flakes"));
+		topRange.add(toppings.getNoneOption());
+		toppings.setRange(topRange);
+		toppings.setCardinality(new Cardinality(0,4));
+		name=new StringDecision("your name?");	
+		dm.add(cream);
+		dm.add(holder);
+		dm.add(portions);
+		dm.add(flavors);
+		dm.add(name);
+		dm.add(toppings);
+		fm= new FeatureModel();		
 	}
 
 	@Test
-	void testCreateFeatures() {
-		fail("Not yet implemented");
+	void testCreateFeatures() throws CircleInConditionException {
+		TransformDMtoFMUtil.createFeatures(fm,dm);
 	}
 
 	@Test
