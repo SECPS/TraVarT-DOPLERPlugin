ad48d273fdcdbf20c87c06a39b438496d505e312
unknown <yannickkraml@gmail.com>
2025-02-13 14:16:21 +0100

    Fixed Z3RunnerImpl

--- src/main/java/edu/kit/travart/dopler/sampling/Z3RunnerImpl.java
+++ src/main/java/edu/kit/travart/dopler/sampling/Z3RunnerImpl.java
@@ -3,7 +3,6 @@ package edu.kit.travart.dopler.sampling;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.nio.charset.StandardCharsets;
@@ -18,20 +17,29 @@ public class Z3RunnerImpl implements Z3Runner {
 
     @Override
     public Scanner runZ3(Stream<String> z3Input) {
-        try {
-            Process process = new ProcessBuilder().command(Z3_EXE, "-in", "-smt2").start();
-            OutputStream out = process.getOutputStream();
-            InputStream in = process.getInputStream();
+        Process process = startZ3Process();
+        inputData(z3Input, process.getOutputStream());
+        return new Scanner(process.getInputStream(), StandardCharsets.UTF_8);
+    }
 
-            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out, StandardCharsets.UTF_8));
+    private static Process startZ3Process() {
+        Process process;
+        try {
+            process = new ProcessBuilder().command(Z3_EXE, "-in", "-smt2").start();
+        } catch (IOException ioException) {
+            throw new SatException(ioException);
+        }
+        return process;
+    }
+
+    private static void inputData(Stream<String> z3Input, OutputStream out) {
+        try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out, StandardCharsets.UTF_8))) {
             for (String singleInput : z3Input.toList()) {
                 writer.write(singleInput);
                 writer.newLine();
             }
-
-            return new Scanner(in, StandardCharsets.UTF_8);
-        } catch (IOException ioException) {
-            throw new SatException(ioException);
+        } catch (IOException e) {
+            throw new SatException(e);
         }
     }
 }
